c62ae8f56a64f81ecfd87b345dc0f9cc4c709bac
Dzakiy Dzakwan <dzakcart@gmail.com>
2024-11-08 10:32:05 +0700

    python: module

---
+++ Python/chapter-8.py
@@ -0,0 +1,8 @@
+# Import and Module
+import chapter8.chapter_8a as chapter_8a
+from chapter8.chapter_8b import sumarize
+
+print(chapter_8a.data)
+print(chapter_8a.greet("Dzakiy Dzakwan"))
+
+print(sumarize(1, 2, 3))
---
+++ Python/chapter8/chapter_8a.py
@@ -0,0 +1,4 @@
+data = "This data is from chapter 8b file"
+
+def greet(name :str):
+    return f"halo {name}, greeting from 8b"
